0073cf03c97982eb61505304fa67f21af55cc65b
Kaia <kbergmanndean@gmail.com>
2021-09-13 12:07:31 -0400

    solved algorithm

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,21 @@
 function reverseString(str) {
-  // type your code here
+  //make new string
+  let newStr="";
+  //create for loop -beginning of loop will be last character of string, str.length-1.
+  //loop goes as along as i>=0.
+  //decrement after each iteration
+  for(let i=str.length-1;i>=0;i--){
+    newStr += str[i]; //or newStr=newStr+str[i]
+  }
+  return newStr;
+
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 'cba'")
+  console.log("=>", reverseString("abc"))
+
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -16,4 +28,6 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+// Inputs a string, outputs the string with characters in reverse order.
+
 // And a written explanation of your solution
